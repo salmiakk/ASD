256e85cd65edc6790775053db73e0e5dafd8f760
Mateusz Pioch <s21331@pjwstk.edu.pl>
2021-05-21 17:28:30 +0200

    zmiana nazw

--- algorytmy_porownanie.py
+++ algorytmy_porownanie.py
@@ -65,17 +65,17 @@ def calculateExecutionTime(a, f):
 A = [7,10,20,15,4,3,3,8,100,1,7,10,20,15,4,3,3,8,100,1,7,10,20,15,4,3,3,8,100,1,7,10,20,15,4,3,3,8,100,1,7,10,20,15,4,3,3,8,100,1]
 print("Bubble_sort,random", calculateExecutionTime(A, bubbleSort))
 print("Heap_sort,random", calculateExecutionTime(A, heapSort))
-print("Quick_sort,random", calculateExecutionTime(A, bubbleSort))
+print("Quick_sort,random", calculateExecutionTime(A, quickSort))
 
 B = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50]
 print("Bubble_sort,sorted", calculateExecutionTime(B,bubbleSort))
 print("Heap_sort,sorted", calculateExecutionTime(B,heapSort))
-print("Quick_sort,sorted", calculateExecutionTime(B,bubbleSort))
+print("Quick_sort,sorted", calculateExecutionTime(B,quickSort))
 
 C = [50,49,48,47,46,45,44,43,42,41,40,39,38,37,36,35,34,33,32,31,30,29,28,27,26,25,24,23,22,21,20,19,18,17,16,15,14,13,12,11,10,9,8,7,6,5,4,3,2,1]
 print("Bubble_sort,reverse_sorted", calculateExecutionTime(C, bubbleSort))
 print("Heap_sort,reverse_sorted", calculateExecutionTime(C, heapSort))
-print("Quick_sort,reverse_sorted", calculateExecutionTime(C, bubbleSort))
+print("Quick_sort,reverse_sorted", calculateExecutionTime(C, quickSort))
 
 
 # Dla danych posortowanych, najlepiej sprawdza się sortowanie bąbelkowe (w tym algorytmie złożoność przy takim zbiorze danych wynosi O(n)).
